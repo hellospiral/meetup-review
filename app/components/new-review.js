a07960ee12f2f027f028cd0ea7492f877cd7f24e
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-26 15:23:13 -0700

    edit new-review and review-rating-stars to allow user to click on stars to set rating when adding new review

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -3,21 +3,34 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   addNewReview: false,
 
+  ratingProperty: "",
+
+  displayRatingProperty: Ember.computed('ratingProperty', function() {
+    return this.get('ratingProperty.rating');
+  }),
+
   actions: {
     reviewFormShow() {
       this.set('addNewReview', true);
+    },
+
+    updateRating(rating) {
+      this.set('ratingProperty', rating);
     },
 
     save1() {
       var params = {
         username: this.get('username'),
         comment: this.get('comment'),
-        rating: parseInt(this.get('rating')),
+        rating: this.get('ratingProperty.rating'),
         meetup: this.get('meetup'),
         timestamp: Date.now()
       };
       this.set('addNewReview', false);
       this.sendAction('save2', params);
+      this.set('username', "");
+      this.set('comment', "");
+      this.set('ratingProperty', "");
     }
   }
 });
--- app/helpers/review-rating-stars.js
+++ app/helpers/review-rating-stars.js
@@ -4,7 +4,7 @@ export function reviewRatingStars(params) {
   var rating = parseInt(params[0].get('rating'));
   var starsHTML = "";
   for (var i = 0; i < rating; i++) {
-    starsHTML += "<span class='glyphicon glyphicon-star' aria-hidden='true'></span>"
+    starsHTML += "<span class='glyphicon glyphicon-star' aria-hidden='true'></span>";
   }
   return Ember.String.htmlSafe(starsHTML);
 }

7983d421d561d857a7434209d4329362aa462fb6
Matt Carlson <matthewthomascarlson@gmail.com>
2016-08-26 10:30:46 -0700

    add averageRating computed property to review-list component

--- app/components/review-list.js
+++ app/components/review-list.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  averageRating: Ember.computed('meetup.reviews', function() {
+    var reviews = this.get('meetup.reviews');
+    var ratingsArray = reviews.getEach('rating');
+    var total = 0;
+    for (var i = 0; i < ratingsArray.length; i++) {
+      total += ratingsArray[i];
+    }
+    return (total / ratingsArray.length).toFixed([1]);
+  })
 });
